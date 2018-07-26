2582d444503382e9193e53cd9917e9c5ac80d5a6
max <maksymilian.babarowski@gmail.com>
2018-07-26 20:23:36 +0200

    Changed method of assigning labels to config variables

--- php/edit_labels.php
+++ php/edit_labels.php
@@ -3,20 +3,48 @@
 
     function getFieldLabel( $varname ) {
         if( $LANG = "pl_PL" ) {
-            switch( $varname ) {
-                case "issuer_name": return "Nazwa wystawcy";
-                case "issuer_street": return "Adres wystawcy - ulica";
-                case "issuer_zip": return "Adres wystawcy - kod pocztowy";
-                case "issuer_city": return "Adres wystawcy - miasto";
-                case "issuer_iban": return "Numer rachunku wystawcy";
-                case "issuer_swift": return "Kod SWIFT banku wystawcy";
-                case "print_no": return "Ilość cyfr w numerze porządkowym faktury";
-                case "ordinal_no": return "Źródło numeru porządkowego";
-                case "invoice_no": return "Format pełnego numeru faktury";
-                case "buyer_name": return "Nazwa odbiorcy";
-                case "buyer_street": return "Adres odbiorcy - ulica";
-                default: return $varname;
-            }
+            $strings = [
+                "issuer_name" => "Nazwa wystawcy",
+                "issuer_street" => "Adres wystawcy - ulica",
+                "issuer_zip" => "Adres wystawcy - kod pocztowy",
+                "issuer_city" => "Adres wystawcy - miasto",
+                "issuer_iban" => "Numer rachunku wystawcy",
+                "issuer_swift" => "Kod SWIFT banku wystawcy",
+                "issuer_phone" => "Numer telefonu wystawcy",
+                "issuer_tax" => "NIP wystawcy",
+                "print_no" => "Ilość cyfr w numerze porządkowym",
+                "ordinal_no" => "Źródło numeru porządkowego",
+                "invoice_no" => "Format pełnego numeru faktury",
+                "buyer_name" => "Nazwa odbiorcy",
+                "buyer_street" => "Adres odbiorcy - ulica",
+                "buyer_zip" => "Adres odbiorcy - kod pocztowy",
+                "buyer_city" => "Adres odbiorcy - miasto",
+                "buyer_tax" => "NIP odbiorcy",
+                "issue_city" => "Miasto wystawienia",
+                "issue_date" => "Data wystawienia",
+                "service_date" => "Data wykonania usługi",
+                "payment_method" => "Sposób płatności",
+                "deadline_date" => "Ilość dni do terminu zapłaty",
+                "curr" => "Waluta",
+                "comments" => "Uwagi",
+                "item_name" => "Nazwa przedmiotu/usługi",
+                "item_unit" => "Jednostka",
+                "item_quan" => "Ilość",
+                "item_price_netto" => "Cena netto",
+                "item_tax_rate" => "Wartość podatku",
+                "on_day" => "Wystawiana dnia",
+                "rtf_template" => "Szablon faktury",
+                "mail_template" => "Szablon maila",
+                "mail_to" => "Odbiorcy wiadomości",
+                "mail_bbc" => "Odbiorcy wiadomości (UDW)",
+                "mail_subject" => "Temat wiadomości",
+            ];
+        }
+
+        if( array_key_exists( $varname, $strings ) ) {
+            return $strings[ $varname ];
+        } else {
+            return $varname;
         }
     }
 ?>
