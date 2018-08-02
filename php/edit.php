5809abe5d0de07ea946f7d6ec15875333f34ca45
max <maksymilian.babarowski@gmail.com>
2018-08-02 23:20:50 +0200

    Adding new variables on edit page

--- php/edit.php
+++ php/edit.php
@@ -22,6 +22,22 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
    </head>
   <body>
+    <script>
+        var i = 1;
+
+        function addRow() {
+            var left = "Nazwa nowej wartości";
+            var right = "Wartość";
+            
+            var table = document.getElementById("list").getElementsByTagName("tbody")[0];
+            var row = table.insertRow(table.rows.length);
+            var cell1 = row.insertCell(0);
+            var cell2 = row.insertCell(1);
+            cell1.innerHTML = "<input type=\"text\" name=\"" + "val" + i + "\" value=\"" + left + "\" />";
+            cell2.innerHTML = "<input type=\"text\" name=\"" + "var" + i + "\" value=\"" + right + "\" />";
+            i++;
+        }
+    </script>
     <nav class="navbar navbar-default navbar-fixed-top">
         <div class="container-fluid">
           <div class="navbar-header">
@@ -39,7 +55,7 @@
     </nav>
     <div class="container" style="padding-left: 200px; padding-right: 200px; margin-top: 50px;">
         <form action="save.php" method="GET">
-            <table class="table table-bordered">
+            <table class="table table-bordered" id="list">
                 <thead>
                     <tr>
                         <th width="35%">Variable name</th>
@@ -51,14 +67,17 @@
                         foreach( $file_array as $line_number => $line ) {
                             if( $line[ 0 ] != '#' ) {
                                 list( $var, $val ) = explode( '=', $line );
-                                echo "<tr><td>".getFieldLabel( $var );
-                                echo "</td>";
-                                echo "<td><input type=\"text\" name=\"".$var."\" value=\"".trim( $val, "\"")."\" /></td>";
+                                echo "<tr>\n\t<td>".getFieldLabel( $var );
+                                echo "</td>\n";
+                                echo "\t<td><input type=\"text\" name=\"".$var."\" value=\"".trim( $val, "\"")."\" /></td>\n</tr>\n";
                             }
                         }
                     ?>
                 </tbody>
             </table>
+            <div class="pull-left">
+                <input type="button" class="btn btn-info" value="Add row" onclick="addRow();" />
+            </div>
             <div class="pull-right">
                 <input type="submit" class="btn btn-info" value="Save" style="text-align: right;">
             </div>
